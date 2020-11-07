4ce00e80c9fbc098cb09e7411fc366ba0619bc8d
se9oo <se9oo@kakao.com>
2020-11-07 17:17:58 +0900

    page title 추가

--- Se9ooGG/front/pages/editPost.js
+++ Se9ooGG/front/pages/editPost.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Head from 'next/head';
 import AppLayout from '../components/AppLayout';
 import PostForm from '../components/PostForm';
 
 const editPost = () => {
   return (
     <AppLayout>
+      <Head>
+        <title>se9oo | 글 등록</title>
+      </Head>
       <PostForm />
     </AppLayout>
   );
