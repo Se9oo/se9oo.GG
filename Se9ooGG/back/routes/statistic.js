2eedf95f871ccc02fd9093f3210d0c7e147d948e
park se hyeon <se9oo@kakao.com>
2020-12-04 23:44:53 +0900

    소환사 및 숙련도 top3 챔피언 정보 가져오기 추가

--- Se9ooGG/back/routes/statistic.js
+++ Se9ooGG/back/routes/statistic.js
@@ -6,26 +6,41 @@ const router = express.Router();
 
 dotenv.config();
 
-router.post('/statistic/loadSummoner', (req, res, next) => {
+router.post('/statistic/loadSummoner', async (req, res, next) => {
   const { summonerName } = req.body;
   
-  if (summonerName) {
-    axios.get(encodeURI(`https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerName}?api_key=${process.env.API_KEY}`))
-    .then((response) => {
-      return res.status(200).json([response.data][0].accountId);
-    })
-    .catch(function (err) {
-      console.log(err);
-    });
-  } else {
-    return res.status(401).json('error');
+  try {
+     if (summonerName) {
+      // 소환사 이름으로 정보 찾기
+      const summonerBaseInfo = await axios.get(encodeURI(`https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerName}?api_key=${process.env.API_KEY}`));
+      const { id, profileIconId, summonerLevel, accountId } = summonerBaseInfo.data;
+
+      if (!summonerBaseInfo) {
+        return res.status(401).json('사용자 정보를 확인해주세요.');
+      }
+
+      // 소환사 숙련도 top3 챔피언 정보
+      const proficiencyLevelInfo = await axios.get(`https://kr.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/${id}?api_key=${process.env.API_KEY}`);
+      const proficiencyTop3Info = proficiencyLevelInfo.data.slice(0, 3);
+      
+      // delete summonerId
+      proficiencyTop3Info.map((v) =>  delete v.summonerId);
+      
+      const summonerData = {
+        profileIconId: profileIconId,
+        summonerName: summonerName,
+        summonerLevel: summonerLevel,
+        proficiencyLevelInfo: proficiencyTop3Info,
+      }
+
+      return res.status(200).json(summonerData);
+     } else {
+       return res.status(401).json('사용자 정보를 확인해주세요.');
+     }
+  } catch (err) {
+    console.error(err);
+    next(err);
   }
-});
-
-router.post('/statistic/loadSummonerInfo', (req, res, next) => {
-  const { accountId } = req.body;
-
-  return res.status(200).json('success');
 });
 
 module.exports = router;
--- Se9ooGG/front/sagas/statistic.js
+++ Se9ooGG/front/sagas/statistic.js
@@ -3,11 +3,7 @@ import { all, call, fork, put, takeLatest } from 'redux-saga/effects';
 import { LOAD_SUMMONER_FAILURE, LOAD_SUMMONER_REQUEST, LOAD_SUMMONER_SUCCESS } from '../reducer/statistic';
 
 function loadSummonerAPI (data) {
-  axios.post('/statistic/loadSummoner', data)
-  .then((response) => {
-    const accountId = { accountId: response.data }
-    return axios.post('/statistic/loadSummonerInfo', accountId);
-  });
+  return axios.post('/statistic/loadSummoner', data)
 }
 
 function* loadSummoner(action) {
@@ -15,7 +11,7 @@ function* loadSummoner(action) {
     const result = yield call(loadSummonerAPI, action.data);
     yield put({
       type: LOAD_SUMMONER_SUCCESS,
-      data: result,
+      data: result.data,
     });
   } catch (err) {
     yield put({
