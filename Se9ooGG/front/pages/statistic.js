449e61c993f39acc1e38e9df636d434e9dd7b673
park se hyeon <se9oo@kakao.com>
2020-12-18 21:45:46 +0900

    전적 새로고침, 전적페이지에서 사용자 검색 기능 추가

--- Se9ooGG/front/pages/statistic.js
+++ Se9ooGG/front/pages/statistic.js
@@ -1,17 +1,22 @@
-import React, { useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import React, { useCallback, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { loadSummonerDoneClearAction, loadSummonerRequestAction } from '../reducer/statistic';
 import Router from 'next/router';
 import AppLayout from '../components/AppLayout';
 import SummonerRankItem from '../components/SummonerRankItem';
 import SummonerMostChampionItem from '../components/SummonerMostChampionItem';
 import SummonerMatchItem from '../components/SummonerMatchItem';
-import { Button } from 'antd';
+import { Button, Input } from 'antd';
 import styled from 'styled-components';
 import { errorModal } from '../components/CommonModal';
+import useInput from '../hooks/useInput';
 
 const Statistic = () => {
-  const { summoner } = useSelector((state) => state.statistic);
+  const dispatch = useDispatch('');
+  const { summoner, loadSummonerDone } = useSelector((state) => state.statistic);
+  const [search, onSearchInput] = useInput('');
 
+  // 예외처리
   useEffect(() => {
     if (Object.keys(summoner).length == 0) {
       errorModal(`사용자를 검색해주세요.\n메인 화면으로 이동합니다.`);
@@ -19,15 +24,44 @@ const Statistic = () => {
     }
   }, [summoner])
 
+  // 사용자 전적 검색 후 loadSummonerDone state 초기화
+  useEffect(() => {
+    if (loadSummonerDone) {
+      dispatch(loadSummonerDoneClearAction());
+    }
+  }, [loadSummonerDone])
+
+  // 사용자 전적 검색
+  const onSubmitInput = useCallback(() => {
+    dispatch(loadSummonerRequestAction({
+      summonerName: search
+    }));
+  }, [search])
+
+  // 전적 새로고침
+  const onClickRefresh = useCallback(() => {
+    dispatch(loadSummonerRequestAction({
+      summonerName: summoner.summonerName
+    }));
+  }, [summoner]);
+
   return (
     <AppLayout>
       {
         Object.keys(summoner).length != 0
         ?
         <>
+          <UserSearchInput 
+            placeholder="사용자명을 입력하세요."
+            onSearch={onSubmitInput}
+            onChange={onSearchInput}
+            value={search}
+            enterButton
+          />
           <Summoner>
             <img src={`./img/profileicon/${summoner.profileIconId}.png`} alt="summoner-profile-icon"/>
             <strong>{summoner.summonerName}</strong>
+            <Button onClick={onClickRefresh}>전적 새로고침</Button>
             <Button type="primary">인게임 정보</Button>
           </Summoner>
           <SummonerRank>
@@ -62,6 +96,14 @@ const Statistic = () => {
   );
 };
 
+const UserSearchInput = styled(Input.Search)`
+  max-width: 100%;
+
+  & ::placeholder {
+    font-size: 1.2rem;
+  }
+`;
+
 export const Summoner = styled.div`
   display: flex;
   flex-direction: column;
@@ -75,6 +117,7 @@ export const Summoner = styled.div`
   & img {
     width: 30%;
     height: 30%;
+    border-radius: 999px;
   }
 
   & strong {
