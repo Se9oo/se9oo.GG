c8995901de4e4c5ade5e1cc6da245826aba0c26b
park se hyeon <se9oo@kakao.com>
2020-12-27 20:49:15 +0900

    match detail total 챔피언 이미지 추가

--- Se9ooGG/front/components/MatchDetailTotalSummoner.js
+++ Se9ooGG/front/components/MatchDetailTotalSummoner.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import styled from 'styled-components';
+import { getChampionNameById } from './JsonUtil';
 
 const MatchDetailTotalSummoner = ({ gameInfo }) => {
-  
+  const championName = getChampionNameById(gameInfo.championId);
   return (
-    <li>
-      {gameInfo.championId}
-    </li>
+    <SummonerListItem>
+      <img src={`./img/champion/${championName.eng}.png`}/>
+    </SummonerListItem>
   );
 };
 
 export default MatchDetailTotalSummoner;
+
+const SummonerListItem = styled.li`
+  width: 100%;
+  padding: .5rem 1rem;
+
+  & img {
+    width: 10%;
+    border-radius: 999px;
+  }
+`;
