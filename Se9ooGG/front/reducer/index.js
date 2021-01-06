1ee2e7a4a796186ee73c60dcb09a658cef86ef6a
park se hyeon <se9oo@kakao.com>
2021-01-06 23:31:12 +0900

    rootReducer 수정 > index state 안에 state 중복 현상 수정

--- Se9ooGG/front/reducer/index.js
+++ Se9ooGG/front/reducer/index.js
@@ -4,20 +4,20 @@ import user from './user';
 import post from './post';
 import statistic from './statistic';
 
-const rootReducer = combineReducers({
-  index: (state = {}, action) => {
-    switch (action.type) {
-      case HYDRATE:
-        //console.log('HYDRATE', action);
-        return { ...state, ...action.payload};
-
-      default:
-        return state;
+const rootReducer = (state, action) => {
+  switch (action.type) {
+    case HYDRATE:
+      //console.log('HYDRATE', action);
+      return action.payload;
+    default: {
+      const combinedReducer = combineReducers({
+        user,
+        post,
+        statistic,
+      });
+      return combinedReducer(state, action);
     }
-  },
-  user,
-  post,
-  statistic,
-});
+  }
+};
 
 export default rootReducer;
