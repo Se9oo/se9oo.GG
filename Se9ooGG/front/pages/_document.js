1dead18d3990122a13c8fde1367cdc261d855577
park se hyeon <se9oo@kakao.com>
2021-01-09 20:31:36 +0900

    _document.js return 추가

--- Se9ooGG/front/pages/_document.js
+++ Se9ooGG/front/pages/_document.js
@@ -29,13 +29,15 @@ export default class extends Document {
   }
 
   render() {
-    <Html>
-      <Head />
-      <body>
-        <Main />
-        <script src="https://polyfill.io/v3/polyfill.min.js?features=default%2Ces2015%2Ces2016%2Ces2017%2Ces2018%2Ces2019" />
-        <NextScript />
-      </body>
-    </Html>
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <script src="https://polyfill.io/v3/polyfill.min.js?features=default%2Ces2015%2Ces2016%2Ces2017%2Ces2018%2Ces2019" />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 }
