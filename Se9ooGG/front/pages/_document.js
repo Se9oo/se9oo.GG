c05b6b8d2e1308e83c42eec662ccaed0ac402bed
park se hyeon <se9oo@kakao.com>
2021-01-10 20:11:53 +0900

    polyfill 수정

--- Se9ooGG/front/pages/_document.js
+++ Se9ooGG/front/pages/_document.js
@@ -34,7 +34,7 @@ export default class MyDocument extends Document {
         <Head />
         <body>
           <Main />
-          <script src="https://polyfill.io/v3/polyfill.min.js?features=default%2Ces2015%2Ces2016%2Ces2017%2Ces2018%2Ces2019" />
+          <script src="https://polyfill.io/v3/polyfill.min.js?features=es2015%2Cdefault%2Ces2016%2Ces2017%2Ces2018%2Ces2019%2Ces5%2Ces6" />
           <NextScript />
         </body>
       </Html>
