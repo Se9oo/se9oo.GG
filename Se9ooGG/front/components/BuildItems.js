728a627cef49cd9291453a12cf26bbfaf3fbfa39
park se hyeon <se9oo@kakao.com>
2021-01-17 23:04:37 +0900

    match build > 구매한 아이템 이미지 추가

--- Se9ooGG/front/components/BuildItems.js
+++ Se9ooGG/front/components/BuildItems.js
@@ -1,15 +1,55 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const BuildItems = ({ items, time }) => {
+const BuildItems = ({ items }) => {
+  // 되돌린 item list
+  const undoItems = [];
+  items.map((undo) => {
+    if (undo.type === 'ITEM_UNDO') {
+      undoItems.push(undo.beforeId);
+    }
+  });
+
+  let undoCount = undoItems.length;
+  let finalItemArr = [];
+  if (undoItems.length !== 0) {
+     items.map((item) => {
+      if (item.type === 'ITEM_PURCHASED') {
+        if (undoCount === 0) {
+          finalItemArr.push(item);
+        } else {
+          if (undoItems.includes(item.itemId)) {
+            undoCount--;
+          } else {
+            finalItemArr.push(item);
+          }
+        }
+      }
+    });
+  } else {
+    finalItemArr = [...items];
+  }
+  
   return (
-    <>
+    <Item>
       {
-        items.map((i) => {
-          return <div>{`${time} : ${i.type}`}</div>
+        finalItemArr.map((i) => {
+          if (i.type === 'ITEM_PURCHASED') {
+            return <ItemImg src={`/img/item/${i.itemId}.png`} />
+          }
         })
       }
-    </>
+    </Item>
   );
 };
 
 export default BuildItems;
+
+const Item = styled.div`
+  display: flex;
+`;
+
+const ItemImg = styled.img`
+  width: 50px;
+  height: 50px;
+`;
