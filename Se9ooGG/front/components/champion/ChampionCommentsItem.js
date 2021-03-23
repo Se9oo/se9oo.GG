d256ef9f44826b92ad6c9c64ad1db7555f1823c5
park se hyeon <se9oo@kakao.com>
2021-03-23 22:42:19 +0900

    챔피언 정보 > 한줄평 > 한줄평 취소 모달창 추가

--- Se9ooGG/front/components/champion/ChampionCommentsItem.js
+++ Se9ooGG/front/components/champion/ChampionCommentsItem.js
@@ -1,25 +1,51 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { cancelChampionCommentAction } from '../../reducer/champion';
+import CommonModal from '../CommonModal';
 import { CloseOutlined } from '@ant-design/icons';
 import styled from 'styled-components';
 
 const ChampionCommentsItem = ({ comment }) => {
   const { me } = useSelector((state) => state.user);
   const dispatch = useDispatch('');
+  // modal 노출 여부
+  const [showModal, setShowModal] = useState(false);
+  // modal content
+  const [modalContent, setModalContent] = useState('');
 
-  const onClickCancelBtn = useCallback(() => {
+  // 한줄평 삭제 modal ok
+  const onOkCancelComment = useCallback(() => {
     dispatch(cancelChampionCommentAction({ commentId: comment.commentId }));
+    setShowModal(false);
+  }, [comment.commentId]);
+
+  // 한줄평 삭제 modal cancel
+  const onCancelCancelComment = useCallback(() => {
+    setShowModal(false);
+  }, []);
+
+  // 한줄평 삭제
+  const onClickCancelBtn = useCallback(() => {
+    setModalContent({
+      title: '한줄평 삭제',
+      onOk: onOkCancelComment,
+      onCancel: onCancelCancelComment,
+      content: '해당 한줄평을 삭제하시겠습니까?',
+    });
+    setShowModal(true);
   }, []);
 
   return (
-    <Comment>
-      <CommentItem>
-        <Content>{comment.content}</Content>
-        {me && me.email === comment.userEmail && <CommentCancelButton onClick={onClickCancelBtn} />}
-      </CommentItem>
-      <UserName>{`- ${comment.userNickname} -`}</UserName>
-    </Comment>
+    <>
+      <Comment>
+        <CommentItem>
+          <Content>{comment.content}</Content>
+          {me && me.email === comment.userEmail && <CommentCancelButton onClick={onClickCancelBtn} />}
+        </CommentItem>
+        <UserName>{`- ${comment.userNickname} -`}</UserName>
+      </Comment>
+      <CommonModal modalContent={modalContent} visible={showModal} />
+    </>
   );
 };
 
